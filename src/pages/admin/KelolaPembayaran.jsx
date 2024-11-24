f5a598f064e49647f03786df4861e4a022e79c55
zanafiii <azzam.emirates@gmail.com>
2024-11-24 19:29:33 +0700

    remove unused comment on KelolaPembayaran.jsx

--- src/pages/admin/KelolaPembayaran.jsx
+++ src/pages/admin/KelolaPembayaran.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  getJumlahTagihanPending,
-  getPembayaran,
-} from "../../services/AdminService";
+import { getJumlahTagihanPending, getPembayaran } from "../../services/AdminService";
 
 const KelolaPembayaran = () => {
   const [payments, setPayments] = useState([
@@ -33,16 +30,10 @@ const KelolaPembayaran = () => {
   ]);
 
   const handleAction = (id, action) => {
-    setPayments((prevPayments) =>
-      prevPayments.map((payment) =>
-        payment.id === id ? { ...payment, status: action } : payment
-      )
-    );
+    setPayments((prevPayments) => prevPayments.map((payment) => (payment.id === id ? { ...payment, status: action } : payment)));
   };
 
-  const pendingPaymentsCount = payments.filter(
-    (payment) => payment.status === null
-  ).length;
+  const pendingPaymentsCount = payments.filter((payment) => payment.status === null).length;
 
   const [jumlahTagihanPending, setJumlahTagihanPending] = useState(0);
   const [getTagihan, setPembayaran] = useState([]);
@@ -56,10 +47,7 @@ const KelolaPembayaran = () => {
         const data = await getJumlahTagihanPending();
         setJumlahTagihanPending(data.jumlah_tagihan_pending); // Update state dengan data dari API
       } catch (error) {
-        console.error(
-          "Terjadi kesalahan saat mengambil data jumlah kelas aktif:",
-          error
-        );
+        console.error("Terjadi kesalahan saat mengambil data jumlah kelas aktif:", error);
       }
     };
 
@@ -80,34 +68,6 @@ const KelolaPembayaran = () => {
     fetchData();
   }, []);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const data = await getPembayaran();
-  //       setPembayaran(data);
-  //     } catch (err) {
-  //       setError("Gagal memuat data pembayaran.");
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-  //   useEffect(() => {
-  //     const fetchData = async () => {
-  //         try {
-  //             const data = await getTagihan(); // Panggil API
-  //             setTagihan(data); // Simpan data ke state
-  //         } catch (error) {
-  //             console.error("Terjadi kesalahan saat mengambil data:", error);
-  //         } finally {
-  //             setIsLoading(false); // Matikan loading
-  //         }
-  //     };
-
-  //     fetchData();
-  // }, []);
-
   return (
     <div className="p-6 bg-gray-100 h-full flex flex-col gap-6">
       {/* Header */}
@@ -118,20 +78,14 @@ const KelolaPembayaran = () => {
       {/* Data */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="bg-white p-6 rounded-lg shadow text-center">
-          <h3 className="text-[#ff8c00] font-bold text-lg">
-            Pembayaran Menunggu Verifikasi
-          </h3>
-          <p className="text-[#ff8c00] text-4xl font-bold">
-            {jumlahTagihanPending}
-          </p>
+          <h3 className="text-[#ff8c00] font-bold text-lg">Pembayaran Menunggu Verifikasi</h3>
+          <p className="text-[#ff8c00] text-4xl font-bold">{jumlahTagihanPending}</p>
         </div>
       </div>
 
       {/* Tabel Daftar Pembayaran */}
       <div className="bg-white p-6 rounded-lg shadow">
-        <h3 className="text-[#212121] font-bold text-lg mb-4">
-          Daftar Pembayaran
-        </h3>
+        <h3 className="text-[#212121] font-bold text-lg mb-4">Daftar Pembayaran</h3>
         {error ? (
           <p className="text-red-500">{error}</p>
         ) : (
@@ -151,39 +105,20 @@ const KelolaPembayaran = () => {
               <tbody>
                 {getTagihan.map((item) => (
                   <tr key={item.id_pembayaran}>
-                    <td className="py-2 px-4 border-b text-center">
-                      {item.id_pembayaran}
-                    </td>
+                    <td className="py-2 px-4 border-b text-center">{item.id_pembayaran}</td>
                     <td className="py-2 px-4 border-b">{item.nama_siswa}</td>
-                    <td className="py-2 px-4 border-b text-center">
-                      {item.tipe_kelas}
-                    </td>
-                    <td className="py-2 px-4 border-b text-center">
-                      {item.tipe_pembayaran}
-                    </td>
+                    <td className="py-2 px-4 border-b text-center">{item.tipe_kelas}</td>
+                    <td className="py-2 px-4 border-b text-center">{item.tipe_pembayaran}</td>
                     <td className="py-2 px-4 border-b text-right">
                       {parseFloat(item.jumlah).toLocaleString("id-ID", {
                         style: "currency",
                         currency: "IDR",
                       })}
                     </td>
-                    <td
-                      className={`py-2 px-4 border-b text-center ${
-                        item.status === "lunas"
-                          ? "text-green-500"
-                          : "text-red-500"
-                      }`}
-                    >
-                      {item.status}
-                    </td>
+                    <td className={`py-2 px-4 border-b text-center ${item.status === "lunas" ? "text-green-500" : "text-red-500"}`}>{item.status}</td>
                     <td className="py-2 px-4 border-b text-center">
                       {item.link_bukti ? (
-                        <a
-                          href={item.link_bukti}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="text-blue-500 underline"
-                        >
+                        <a href={item.link_bukti} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
                           Lihat Bukti
                         </a>
                       ) : (
